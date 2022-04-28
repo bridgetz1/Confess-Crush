1b83aa1203c22a00d8bb44ad369e1233bb254b06
Bridget <93377182+bridgetz1@users.noreply.github.com>
2022-04-29 03:49:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,25 @@ const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đườngconst  CONFIG  =  {
+    titleWeb : "Code tặng eiu huỳnn" ,
+    introTitle : 'LÊ KHÁNH HUYỀN' ,
+    introDesc : Thường lệ vốn trái đất
     Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương` ,
+    btnIntro : '^^ HiHi ^^' ,
+    title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 🥰' ,
+    desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
+    btn Có : 'Thích lắm <33' ,
+    btnNo : 'Không nha: 3' ,
+    question : 'Trên thế giới hơn 7 tỉ lệ người sao bạn lại yêu mình <3' ,
+    btnReply : 'Gửi cho bạn <3' ,
+    reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
+    mess : 'Mình biết mà 🥰. Yêu bạn nhiều 😘😘 ' ,
+    messDesc : 'Thứ 3  a qua đón đi chơi nha. " ,
+    btn Chấp nhận : 'Okiiiii lun <3' ,
+    messLink : 'https://www.facebook.com/ThaiDeepTry.Off/'  
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
